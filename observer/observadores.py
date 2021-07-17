bebae8af4acfc48ba5f3bcbafea445b8938b20e5
Matheus Alberto <maatheusaalberto@gmail.com>
2021-07-17 14:22:48 -0300

    Observer

---
+++ observer/observadores.py
@@ -0,0 +1,8 @@
+def imprime(nota_fiscal):
+    print(f'Imprimindo nota fiscal {nota_fiscal.cnpj}')
+    
+def envia_por_email(nota_fiscal):
+    print(f'Enviando por e-mail nota fiscal {nota_fiscal.cnpj}')
+    
+def salva_no_banco(nota_fiscal):
+    print(f'Salvando nota fiscal {nota_fiscal.cnpj}')
